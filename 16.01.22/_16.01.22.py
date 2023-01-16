7b06651c2d4358fdace5dbfcce8854651d7b5c50
Siil777 <118188578+Siil777@users.noreply.github.com>
2023-01-16 20:58:52 +0200

    Update _16.01.22.py

--- 16.01.22/_16.01.22.py
+++ 16.01.22/_16.01.22.py
@@ -2,33 +2,30 @@
 
 #username=['username1','username2','username3']
 #pass2=['use1','use2','use3']
-#pass1=password(6)
-#while True:
-#        pasword=input('pasword:')
-#        a= True
-#        if len(pasword)<6:
-#            a= False
-#            print('not suitable')
-#        elif  pasword !=pasword2:
-#            a=False 
-#            print('paswords are not iqual')
-#        else:
-#            print('OKI')
-#            while True:
-#                user_name=input('username:')
-#                pasword=input('pasword:')
-#                if user_name==password:
-#                    print('ok')
-#                    a= True
-#                if len(pasword)<6:
-#                    a= False
-#                    print('not suitable')
-#                elif  pasword !=pasword2:
-#                    a=False 
-#                    print('paswords are not iqual')
-#                else:
-#                     print('OKI')
-            
+
+pass1=password(6)
+while True:
+	password=input('password')
+#	password2=input('password')
+	a=True
+	if len(password)<6:
+		a=False
+		break
+		print('has to consost no less then 6 sym')
+#	elif password!=password2:
+#		print('different')
+	user=username(3)
+	while True:
+		user_name=input('username:')
+		print(f'username and paasword',user_name,password)
+		loginpass=askPassword
+		while True:
+		  		login =    input('login')
+		  		password = input('password')
+		  		if login==user_name or password==password:
+		  			print('success')
+		  		else:
+		  			print('failure')           
 
 
 print('Enter or Registration!')
