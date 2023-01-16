557a5c4af0923c3a10ca6e36fd539fb0b208a2c9
opilane <opilane@HP-CZC2349HTD>
2023-01-16 17:28:58 +0200

    Add project files.

---
+++ 16.01.22/_16.01.22.py
@@ -0,0 +1,40 @@
+﻿from module1 import*
+
+#username=['username1','username2','username3']
+#pass2=['use1','use2','use3']
+#pass1=password(6)
+#while True:
+#        pasword=input('pasword:')
+#        a= True
+#        if len(pasword)<6:
+#            a= False
+#            print('not suitable')
+#        elif  pasword !=pasword2:
+#            a=False 
+#            print('paswords are not iqual')
+#        else:
+#            print('OKI')
+#            while True:
+#                user_name=input('username:')
+#                pasword=input('pasword:')
+#                if user_name==password:
+#                    print('ok')
+#                    a= True
+#                if len(pasword)<6:
+#                    a= False
+#                    print('not suitable')
+#                elif  pasword !=pasword2:
+#                    a=False 
+#                    print('paswords are not iqual')
+#                else:
+#                     print('OKI')
+            
+
+
+print('Enter or Registration!')
+pass1=input('Enter/Registration: ')
+if pass1==('Enter') or pass1==('Enter'):
+    vh=input('Enter login and password: ')
+elif pass1==('Registration') or pass1==('Registration'):
+    reg=input('Creat login and password): ') 
+    print(reg)
---
+++ 16.01.22/module1.py
@@ -0,0 +1,48 @@
+﻿from math import*
+import random
+from random import choice
+from re import*
+import string
+def password(a:int):
+    """enter a password 
+    """
+
+    pass1= ""
+    for i in range(a):
+        t=choice(string.ascii_letters) #AA...zz
+        num=choice([1,2,3,4,5,6,7,8,9])
+        
+        t_num=[t,str(num)]
+        pass1+=choice(t_num)
+    return pass1
+
+
+
+username=['username1','username2','username3']
+pass2=['use1','use2','use3']
+def username(b:str):
+    user_name=" "
+    for i in range(b):
+        user_name=input('username:')
+        if user_name==username:
+            print('OKI')
+
+
+
+
+    #str0=".,:;!_*-+()/#¤%&"
+    #str1 = '0123456789'
+    #str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    #str3 = str2.upper()
+    #print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    #str4 = str0+str1+str2+str3
+    #print(str4)
+    #ls = list(str4)
+    #print(ls)
+    #random.shuffle(ls)
+    #print(ls)
+    ## Извлекаем из списка 12 произвольных значений
+    #psword = '12345'.join([random.choice(ls) for x in range(12)])
+    ## Пароль готов
+    #print(psword)
+  # Option 1
